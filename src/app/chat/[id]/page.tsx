a1ffdc236fc8c334637eb7006dc7cd90b12c558e
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-12-28 21:59:06 -0500

    refactor SharedChatPage to mark component as server and prepare for initial data fetching

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -13,6 +13,12 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-export default function Page(props: Props) {
-  return <ChatInterface initialConversationId={props.params.id} />
+// Mark as server component
+export const dynamic = 'force-dynamic';
+
+export default async function Page(props: Props) {
+  // Could fetch initial data here if needed
+  // const data = await fetchInitialData(props.params.id);
+  
+  return <ChatInterface initialConversationId={props.params.id} />;
 }
